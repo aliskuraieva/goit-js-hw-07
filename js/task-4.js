7e17eb21f3531992daac81df3706967018646289
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-07-23 13:00:23 +0300

    add tasks 4-6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+  const formData = {
+    email: email,
+    password: password,
+  };
+  console.log(formData);
+  form.reset();
+};
+
+form.addEventListener("submit", onFormSubmit);
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+const onBtnClick = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+};
+btnEl.addEventListener("click", onBtnClick);
